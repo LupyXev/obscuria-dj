4ee50d30d5ed6f093aeb262dbd3396f0a7018eba
LupyXev <LupyXev@github.com>
2020-08-09 14:49:23 +0200

    c

--- .gitignore/Events/messageReactionAdd.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const Discord = require("discord.js");
-let db = new sqlite3.Database('data.db');
-
-module.exports = async(client, messageReaction, user) => {
-    
-}
-
