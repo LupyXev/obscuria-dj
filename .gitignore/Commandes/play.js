d23f5744cfcee736a5f6fd2817236f46ec015beb
LupyXev <LupyXev@github.com>
2020-08-09 14:55:33 +0200

    Update play.js

--- .gitignore/Commandes/play.js
+++ .gitignore/Commandes/play.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-    message.channel.send(args)
+    message.channel.send("Je lance la playlist ${args[1]}")
 };
 
 module.exports.help = {
