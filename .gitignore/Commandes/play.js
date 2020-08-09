39a1dbe34188f8d0afa3c0fa70a3cb25448bd783
LupyXev <LupyXev@github.com>
2020-08-09 15:25:40 +0200

    Update play.js

--- .gitignore/Commandes/play.js
+++ .gitignore/Commandes/play.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js');
 module.exports.run = (client, message, args) => {
     var musiques = [];
     var bon = false;
-    if(args[1] == "test") {var musiques = ["j'aime les patates"]; bon = true;}
+    if(args[1] == "test") {var musiques = ["j'aime les patates", "coucou", "c'est moi"]; bon = true;}
     else {message.channel.send(`Erreur : playlist *"${args[1]}"* inconnue`);}
     if (bon) {
     
 	message.channel.send(`Je lance la playlist *"${args[1]}"* length : ${musiques.length}`);
 	var i = 0;
-	while (i < musiques.length) {message.channel.send(`-play`); i++}
+	while (i < musiques.length) {message.channel.send(`-play ${musiques[i]`); i++}
     }
 };
 
